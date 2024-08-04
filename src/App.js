99ba0ff15e4f4c002c6f240e4ca702d1f7eb56d2
FRED KANEZA <95358548+amankanz@users.noreply.github.com>
2024-08-04 17:39:43 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [color, setColor] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <h1>Color Picker</h1>
+      <input
+        type="text"
+        value={color}
+        placeholder="Enter a valid color value"
+        onChange={(e) => setColor(e.target.value)}
+      />
+      <section
+        style={{ backgroundColor: color, width: "100vw", height: "50vh" }}
+      >
+        Color will appear here.
+      </section>
+    </main>
   );
 }
 
